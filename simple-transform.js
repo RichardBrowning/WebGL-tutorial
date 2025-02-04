ae0acce06b87ffd95d96bb5d8e4e3b9f6498ffdc
guo.2154 <guo.2154@cse-anc240738l.mynetworksettings.com>
2025-02-03 22:59:01 -0500

    glmatrix

--- simple-transform.js
+++ simple-transform.js
@@ -12,165 +12,165 @@ var draw_type=2;
 
 //////////// Init OpenGL Context etc. ///////////////
 
-    function initGL(canvas) {
-        try {
-            gl = canvas.getContext("experimental-webgl");
-            gl.viewportWidth = canvas.width;
-            gl.viewportHeight = canvas.height;
-        } catch (e) {
-        }
-        if (!gl) {
-            alert("Could not initialise WebGL, sorry :-(");
-        }
+function initGL(canvas) {
+    try {
+        gl = canvas.getContext("experimental-webgl");
+        gl.viewportWidth = canvas.width;
+        gl.viewportHeight = canvas.height;
+    } catch (e) {
     }
-
-
-
-
-    ///////////////////////////////////////////////////////////////
-    ///////////////////////////////////////////////////////////
-
-    var squareVertexPositionBuffer;
-    var squareVertexColorBuffer;
-
-   ////////////////    Initialize VBO  ////////////////////////
-
-    function initBuffers() {
-
-        squareVertexPositionBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
-        vertices = [
-            0.5,  0.5,  0.0,
-		    -0.5,  0.5,  0.0, 
-            -0.5, -0.5,  0.0,
-	        0.5, -0.5,  0.0,
-
-        ];
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-        squareVertexPositionBuffer.vertexSize = 3;
-        squareVertexPositionBuffer.numVertices = 4;
-
-        squareVertexColorBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexColorBuffer);
-        var colors = [
-            1.0, 0.0, 0.0,
-            0.0, 1.0, 0.0,
-            0.0, 0.0, 1.0, 
-            1.0, 0.0, 0.0, 
-        ];
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-        squareVertexColorBuffer. vertexSize = 3;
-        squareVertexColorBuffer.numVertices = 4;
-
+    if (!gl) {
+        alert("Could not initialise WebGL, sorry :-(");
     }
-
-    ///////////////////////////////////////////////////////////////
-    ///////////////////////////////////////////////////////////////
+}
 
 
-    var mvMatrix = mat4.create();
-    var Z_angle = 0.0;
-
-     function degToRad(degrees) {
-        return degrees * Math.PI / 180;
-     }
-
-    ///////////////////////////////////////////////////////////////
-
-    function drawScene() {
-        gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-        mat4.identity(mvMatrix);
-        console.log('Z angle = '+ Z_angle); 
-        mvMatrix = mat4.rotate(mvMatrix, degToRad(Z_angle), [0, 0, 1]); 
-
-        var offset = 0; 
-        var stride = 0; 
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
-        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.vertexSize, gl.FLOAT, false, stride, offset);
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexColorBuffer);
-        gl.vertexAttribPointer(shaderProgram.vertexColorAttribute,squareVertexColorBuffer.vertexSize, gl.FLOAT, false, stride, offset);
-
-       gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
-
-	    if (draw_type==2) gl.drawArrays(gl.TRIANGLE_FAN, 0, squareVertexPositionBuffer.numVertices);
-	    else if (draw_type ==1) gl.drawArrays(gl.LINE_LOOP, 0, squareVertexPositionBuffer.numVertices);	
-        else if (draw_type ==0) gl.drawArrays(gl.POINTS, 0, squareVertexPositionBuffer.numVertices);
-
-    }
 
 
-    ///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 
-     var lastMouseX = 0, lastMouseY = 0;
+var squareVertexPositionBuffer;
+var squareVertexColorBuffer;
 
-    ///////////////////////////////////////////////////////////////
+////////////////    Initialize VBO  ////////////////////////
 
-     function onDocumentMouseDown( event ) {
-          event.preventDefault();
-          document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-          document.addEventListener( 'mouseup', onDocumentMouseUp, false );
-          document.addEventListener( 'mouseout', onDocumentMouseOut, false );
-          var mouseX = event.clientX;
-          var mouseY = event.clientY;
+function initBuffers() {
 
-          lastMouseX = mouseX;
-          lastMouseY = mouseY; 
+    squareVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+    vertices = [
+        0.5,  0.5,  0.0,
+        -0.5,  0.5,  0.0, 
+        -0.5, -0.5,  0.0,
+        0.5, -0.5,  0.0,
 
-      }
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    squareVertexPositionBuffer.vertexSize = 3;
+    squareVertexPositionBuffer.numVertices = 4;
 
-     function onDocumentMouseMove( event ) {
-          var mouseX = event.clientX;
-          var mouseY = event.ClientY; 
+    squareVertexColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexColorBuffer);
+    var colors = [
+        1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0, 
+        1.0, 0.0, 0.0, 
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    squareVertexColorBuffer. vertexSize = 3;
+    squareVertexColorBuffer.numVertices = 4;
 
-          var diffX = mouseX - lastMouseX;
-          var diffY = mouseY - lastMouseY;
+}
 
-          Z_angle = Z_angle + diffX/5;
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
 
-          lastMouseX = mouseX;
-          lastMouseY = mouseY;
 
-          drawScene();
-     }
+var mvMatrix = glMatrix.mat4.create();
+var Z_angle = 0.0;
 
-     function onDocumentMouseUp( event ) {
-          document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-          document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-          document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-     }
+ function degToRad(degrees) {
+    return degrees * Math.PI / 180;
+ }
 
-     function onDocumentMouseOut( event ) {
-          document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-          document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-          document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-     }
+///////////////////////////////////////////////////////////////
 
-    ///////////////////////////////////////////////////////////////
+function drawScene() {
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    function webGLStart() {
-        var canvas = document.getElementById("code03-canvas");
-        initGL(canvas);
-        initShaders();
+    glMatrix.mat4.identity(mvMatrix);
+    console.log('Z angle = '+ Z_angle); 
+    glMatrix.mat4.rotate(mvMatrix, mvMatrix, degToRad(Z_angle), [0, 0, 1]); 
 
-        shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-        gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-        shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
-        gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
-        shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+    var offset = 0; 
+    var stride = 0; 
 
-        initBuffers(); 
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.vertexSize, gl.FLOAT, false, stride, offset);
 
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexColorBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexColorAttribute,squareVertexColorBuffer.vertexSize, gl.FLOAT, false, stride, offset);
 
-       document.addEventListener('mousedown', onDocumentMouseDown,
-       false); 
+   gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
 
-        drawScene();
-    }
+    if (draw_type==2) gl.drawArrays(gl.TRIANGLE_FAN, 0, squareVertexPositionBuffer.numVertices);
+    else if (draw_type ==1) gl.drawArrays(gl.LINE_LOOP, 0, squareVertexPositionBuffer.numVertices); 
+    else if (draw_type ==0) gl.drawArrays(gl.POINTS, 0, squareVertexPositionBuffer.numVertices);
+
+}
+
+
+///////////////////////////////////////////////////////////////
+
+ var lastMouseX = 0, lastMouseY = 0;
+
+///////////////////////////////////////////////////////////////
+
+ function onDocumentMouseDown( event ) {
+      event.preventDefault();
+      document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+      document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+      document.addEventListener( 'mouseout', onDocumentMouseOut, false );
+      var mouseX = event.clientX;
+      var mouseY = event.clientY;
+
+      lastMouseX = mouseX;
+      lastMouseY = mouseY; 
+
+  }
+
+ function onDocumentMouseMove( event ) {
+      var mouseX = event.clientX;
+      var mouseY = event.ClientY; 
+
+      var diffX = mouseX - lastMouseX;
+      var diffY = mouseY - lastMouseY;
+
+      Z_angle = Z_angle + diffX/5;
+
+      lastMouseX = mouseX;
+      lastMouseY = mouseY;
+
+      drawScene();
+ }
+
+ function onDocumentMouseUp( event ) {
+      document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+      document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+      document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+ }
+
+ function onDocumentMouseOut( event ) {
+      document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+      document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+      document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+ }
+
+///////////////////////////////////////////////////////////////
+
+function webGLStart() {
+    var canvas = document.getElementById("code03-canvas");
+    initGL(canvas);
+    initShaders();
+
+    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+    shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
+    gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+    shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+
+    initBuffers(); 
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+   document.addEventListener('mousedown', onDocumentMouseDown,
+   false); 
+
+    drawScene();
+}
 
 function BG(red, green, blue) {
 
